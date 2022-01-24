43478eab589d0ae02da45170a9bbc4ca1b9ad27f
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2022-01-24 18:20:21 -0300

    Creating Modal  and putting to close with esc  Closes #11

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
 import {Container, Content } from './styles' ;
+import Modal from 'react-modal';
 
 
 export function Header() {
+    const [isNewModalTransactionOpen, setIsNewModalTransactionOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewModalTransactionOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {         
+        setIsNewModalTransactionOpen(false);
+    }
+
     return (
-        <Container>
+        <Container> 
             <Content>
            <img src={logoImg} alt="dt money" />
-           <button type="button">
+           <button type="button" onClick={handleOpenNewTransactionModal}>
                Nova transação
            </button>
+
+                <Modal isOpen={isNewModalTransactionOpen} onRequestClose={handleCloseNewTransactionModal}>
+                    <h2>Cadastrar Transação</h2>
+                </Modal>
+
             </Content>
         </Container>
     )
