92c0eea3bba2283688ad3c4d10ff879528ccb546
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2022-01-24 00:13:41 -0300

    First Component - Header Closes #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-
-  button {
-    background: #300;
-  }
-`
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World
-        <button>Salvar</button>
-
-      </Title>
-    </div>
+      <div className="App">
+        <Header />
+          <GlobalStyle />
+    
+      </div>
   );
 }
 
