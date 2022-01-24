be167f593b72cbe9817195458c363c91d78ea2e3
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2022-01-24 18:05:00 -0300

    Using axios to fetch api as service Closes #10

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { Container } from "./styles";
+import {api} from "../../services/api";
 
 
 export function TransactionTable() {
     useEffect(() => {
-      fetch('http://localhost:3000/api/transactions').then(response => response.json())
-      .then(data => console.log(data))
+      api('/transactions')
+      .then(response => console.log(response.data))
     }, [])
 
 
